b04db3f4a49a1103d2a577628be1531b88ce165a
mxber2022 <mxber2022@gmail.com>
2024-01-15 10:53:23 +0100

    mint base wallet setup

---
+++ metagamerhub/src/app/components/Nav.tsx
@@ -0,0 +1,37 @@
+"use client"
+
+import { NearWalletConnector } from "./NearWalletSelector";
+import Link from "next/link";
+
+export default function Nav() {
+    return (
+        <>
+        <nav className="bg-chilizNav rounded-md mx-1 my-1">
+            <div className="flex justify-between px-2">
+              
+              {/* Left Nav */}
+              <div className="flex space-x-4 items-center py-5">
+                <div>
+                    <h1 className="text-3xl font-mono font-bold"><Link href="/">MetaGamerHub</Link></h1>
+                </div>
+              </div>
+              
+              {/* Right Nav */}
+
+              <div className="flex items-center">
+                <div className="bg-gray-300 w-[6rem] flex justify-center rounded-sm text-black mr-10 hover:bg-slate-400">
+                  {/* <Link href="/about">About Us</Link> */}
+                </div>
+
+                <div className="py-5">
+                    <NearWalletConnector />
+                </div>
+
+              </div>
+              
+            </div>
+        </nav>
+            
+        </>
+    )
+}
--- metagamerhub/src/app/page.tsx
+++ metagamerhub/src/app/page.tsx
@@ -1,7 +1,9 @@
+import Nav from "./components/Nav"
+
 export default function Home() {
   return (
     <>
-      helllo
+      <Nav />
     </>
   )
 }
